90a1a73cf0f24f9ae216aacbdf8eb3d24f5e8155
bourneidBH <mbourne@findmymobilehome.com>
2023-10-16 17:13:10 -0500

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION"
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: isProd ? '/sallymccarthylaw' : ''
 }
 
 module.exports = nextConfig
