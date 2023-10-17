7df579a40b6b2c398dbeac7022912c2b6cade516
bourneidBH <mbourne@findmymobilehome.com>
2023-10-17 10:52:48 -0500

    change assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production"
+console.log(isProd)
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/sallymccarthylaw' : '',
+  assetPrefix: isProd ? '/sallymccarthylaw/' : '',
   images: {
     unoptimized: true,
   }
