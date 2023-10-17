012ffedfe54e18b0c36e0dbd063e2df55ffc8b3b
bourneidBH <mbourne@findmymobilehome.com>
2023-10-17 11:00:32 -0500

    add basePath

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production"
-console.log(isProd)
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/sallymccarthylaw/' : '',
+  basePath: isProd ? '/sallymccarthylaw' : '',
+  assetPrefix: isProd ? '/sallymccarthylaw' : '',
   images: {
     unoptimized: true,
   }
